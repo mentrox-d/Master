1159a5aeb624821eb097c99cb5f55d70a7e6a53a
madfuru <100013547+madfuru@users.noreply.github.com>
2026-03-04 09:24:05 +0530

    Update serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,13 +1,33 @@
 from rest_framework import serializers
-from .models import Employee, Attendance
+from .models import Employee
+from django.core.validators import validate_email
+
 
 class EmployeeSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = "__all__"
 
+    # Validate Email Format
+    def validate_email(self, value):
 
-class AttendanceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Attendance
-        fields = '__all__'
+        validate_email(value)
+
+        # Check duplicate email
+        if Employee.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Employee with this email already exists."
+            )
+
+        return value
+
+    # Validate duplicate employee ID
+    def validate_employee_id(self, value):
+
+        if Employee.objects.filter(employee_id=value).exists():
+            raise serializers.ValidationError(
+                "Employee with this ID already exists."
+            )
+
+        return value
